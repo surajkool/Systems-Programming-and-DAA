14b875f4a6d949f0834bc561125328ce6b3915aa
Ishan Joshi <ishanjoshi02@gmail.com>
2018-01-09 19:34:54 +0530

    Added code for N-Queen Problem

---
+++ N-Queen Problem/README.md
@@ -0,0 +1,19 @@
+# Solutions
+
+## Solution for 4 Queens
+
+Solution for 4 Queens is saved in `solution_4.c`
+
+### To run solution for 4 Queens:
+
+1. Compile the code : `gcc solution_4.c`
+2. And run it : `./a.out`
+
+## Solution for 8 Queens
+
+Solution for 4 Queens is saved in `solution_8.c`
+
+### To run solution for 4 Queens:
+
+1. Compile the code : `gcc solution_8.c`
+2. And run it : `./a.out`
---
+++ N-Queen Problem/solution_4.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+#define QUEENS 4
+
+void printBoard(int positions[QUEENS]) {
+	int i, j;
+	for(i = 0;i < QUEENS;i++) {
+		for(j = 0;j < QUEENS;j++) {
+			if (j == positions[i] - 1) {
+				printf("Q\t");
+			} else {
+				printf("0\t");
+			}
+		}
+		printf("\n");
+	}
+}
+
+int checkSolution(int positions[QUEENS]) {
+
+	int i;
+	for(i=0;i<QUEENS;i++) {
+
+		int position = positions[i];
+		int row, column;
+
+		// check attack in column
+		for(row = 1;row<QUEENS;row++) {
+			int k;
+			for(k=0;k<QUEENS;k++) {
+				if(k != i && positions[k] == positions[i]) {
+					return 0;
+				}
+			}
+		}
+		// check attack in diagonals
+		int k, count;
+		for(k = i + 1, count = 1;k < QUEENS;k++, count++) {
+			if (position == positions[k] + count || position == positions[k] - count) {
+				return 0;
+			}
+		}
+
+	}
+
+	return 1;
+
+}
+
+void generateSequence() {
+	int positions[QUEENS];
+	int solutionCount = 1;
+	int i, j, k, l, m, n , o, p;
+	for(i=0;i<QUEENS;i++) {
+		for(j=0;j<QUEENS;j++) {
+			for(k=0;k<QUEENS;k++) {
+				for(l=0;l<QUEENS;l++) {
+            positions[0] = i + 1;
+            positions[1] = j + 1;
+            positions[2] = k + 1;
+            positions[3] = l + 1;
+            int bool = checkSolution(positions);
+            if (bool == 1) {
+            printf("Solution %d\n\n", solutionCount);
+            solutionCount++;
+            printBoard(positions);
+            printf("\n");
+            }
+					}
+				}
+			}
+		}
+	}
+}
+
+int main() {
+
+	generateSequence();
+
+}
